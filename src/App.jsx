73c47fbb3194b46cdb6c4550509816203795fbf4
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-03-24 17:59:23 +0500

    Testing the forked Repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import { basil, alemais } from './assets/images';
+import React from "react";
+import { basil, alemais } from "./assets/images";
 
 function App() {
   return (
     <div>
-      <h1 className='font-social-mono'>Hello World</h1>
-      <h1 className='font-futura'>Hello World</h1>
-      <h1 className='font-helvetica'>Hello World</h1>
-      <h1 className='font-sans'>Hello World</h1>
+      <h1 className="font-social-mono">Hello World</h1>
+      <h1 className="font-futura">Hello World</h1>
+      <h1 className="font-helvetica">Hello World</h1>
+      <h1 className="font-sans">Hello World</h1>
+      <h1>
+        Hello Ali Abbas How are you I have created a new branch and now testing
+        it
+      </h1>
       <img src={basil} alt="" />
       <img src={alemais} alt="" />
     </div>
-  )
+  );
 }
 
 export default App;
